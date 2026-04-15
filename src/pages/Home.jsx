43b541066d04a5c20133699823a8951072977e07
larythonia <larythonia@gmail.com>
2026-04-15 14:54:09 +0100

    login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ function Home()  {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Welcome to My App</h1>
       <p className="text-lg text-gray-600">This is a simple home page.</p>
+      <p>come to my app</p>
     </div>
 
   );
